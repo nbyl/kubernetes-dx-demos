0ba4e923a422cfc87f5f706eefc4940726884916
Nicolas Byl <nico@nicolas-byl.eu>
2018-10-09 01:06:51 +0200

    add application skeleton

---
+++ 03-remote-debugging/src/main/java/de/codecentric/nbyl/kubedx/KubedxApplication.java
@@ -0,0 +1,12 @@
+package de.codecentric.nbyl.kubedx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubedxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubedxApplication.class, args);
+	}
+}
